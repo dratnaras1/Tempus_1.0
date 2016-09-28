7ce464f9e65ebaf7800a38682124cb7021325251
Daniel <dratnaras@gmail.com>
2016-09-28 17:21:38 +0100

    configured with mysql database

--- python3_cal/settings.py
+++ python3_cal/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'python3_cal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Tempus',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '', # Set to empty string for default.
     }
 }
 
